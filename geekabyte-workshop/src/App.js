3ce183ce0761520675d67e780c85da68c60e23b0
Ryan Kretch <rkretch91@gmail.com>
2018-08-25 12:16:11 +0200

    file organizing

--- geekabyte-workshop/src/App.js
+++ geekabyte-workshop/src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabbar from './movie-island/tabbar.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { selectedType: "now_showing"}
+    //By default, now showing is selected
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Ryan's Awesome Movie Site</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">Click on the Following Tabs</p>
-      < div>
-        <span className="tab">Now Showing</span>
-        <span className="tab">Top Rated</span>
-      </div>
+        <p className="App-intro">Click below for some magic</p>
+        <Tabbar/>
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ geekabyte-workshop/src/movie-island/tabbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Tabbar extends Component {
+  constructor(props) {
+    super(props)
+    this.onTabChange = this.onTabChange.bind(this)
+    this.state = { selectedType: "now_showing"}
+    //By default, now showing is selected
+  }
+
+  onTabChange(event) {
+    const selectedType = event.target.getAttribute("data-type")
+    this.setState({
+      selectedType
+    })
+  }
+
+  render() {
+    const { selectedType } = this.state,
+        isNowShowingSelected = selectedType === "now_showing",
+        isTopRatedSelected = selectedType === "top_rated"
+
+    return (
+      <div>
+        <span data-type="now_showing" onClick={this.onTabChange}
+        className= {`tab ${isNowShowingSelected ? 'selected' : ''}`}>Now Showing</span>
+        <span data-type="top_rated" onClick={this.onTabChange}
+        className={`tab ${isTopRatedSelected? 'selected' : ''}`}>Top Rated</span>
+      </div>
+    );
+  }
+}
+
+export default Tabbar
